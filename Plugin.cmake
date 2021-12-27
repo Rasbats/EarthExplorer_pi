81fef345d373e4136608864b3baf1f38f29f1d63
Rasbats <miker@members.swis.net>
2021-12-27 15:13:52 +0000

    Move to managed version

---
+++ Plugin.cmake
@@ -0,0 +1,91 @@
+# ~~~
+# Summary:      Local, non-generic plugin setup
+# Copyright (c) 2020-2021 Mike Rossiter
+# License:      GPLv3+
+# ~~~
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+
+# -------- Options ----------
+
+set(OCPN_TEST_REPO
+    "opencpn/earthexplorer-alpha"
+    CACHE STRING "Default repository for untagged builds"
+)
+set(OCPN_BETA_REPO
+    "opencpn/earthexplorer-beta"
+    CACHE STRING
+    "Default repository for tagged builds matching 'beta'"
+)
+set(OCPN_RELEASE_REPO
+    "opencpn/earthexplorer-prod"
+    CACHE STRING
+    "Default repository for tagged builds not matching 'beta'"
+)
+
+option(EARTHEXPLORER_USE_SVG "Use SVG graphics" ON)
+
+#
+#
+# -------  Plugin setup --------
+#
+set(PKG_NAME EarthExplorer_pi)
+set(PKG_VERSION  3.0.0)
+set(PKG_PRERELEASE "")  # Empty, or a tag like 'beta'
+
+set(DISPLAY_NAME EarthExplorer)    # Dialogs, installer artifacts, ...
+set(PLUGIN_API_NAME EarthExplorer) # As of GetCommonName() in plugin API
+set(PKG_SUMMARY "Connect ship with Google Earth")
+set(PKG_DESCRIPTION [=[
+Connect ship with Google Earth
+]=])
+
+set(PKG_AUTHOR "Mike Rossiter")
+set(PKG_IS_OPEN_SOURCE "yes")
+set(PKG_HOMEPAGE https://github.com/Rasbats/EarthExplorer_pi)
+set(PKG_INFO_URL https://opencpn.org/OpenCPN/plugins/EarthExplorer.html)
+
+set(SRC
+    src/EarthExplorer_pi.h
+    src/EarthExplorer_pi.cpp
+    src/icons.h
+    src/icons.cpp
+    src/EarthExplorergui.h
+    src/EarthExplorergui.cpp
+    src/EarthExplorergui_impl.cpp
+    src/EarthExplorergui_impl.h
+    src/AisMaker.h
+    src/AisMaker.cpp
+    src/GribRecord.cpp
+    src/GribRecordSet.h
+    src/GribRecord.h
+)
+
+set(PKG_API_LIB api-16)  #  A directory in libs/ e. g., api-17 or api-16
+
+macro(late_init)
+  # Perform initialization after the PACKAGE_NAME library, compilers
+  # and ocpn::api is available.
+  if (EARTHEXPLORER_USE_SVG)
+    target_compile_definitions(${PACKAGE_NAME} PUBLIC EARTHEXPLORER_USE_SVG)
+  endif ()
+endmacro ()
+
+macro(add_plugin_libraries)
+  # Add libraries required by this plugin
+  add_subdirectory("libs/tinyxml")
+  target_link_libraries(${PACKAGE_NAME} ocpn::tinyxml)
+
+  add_subdirectory("libs/wxJSON")
+  target_link_libraries(${PACKAGE_NAME} ocpn::wxjson)
+
+  add_subdirectory("libs/plugingl")
+  target_link_libraries(${PACKAGE_NAME} ocpn::plugingl)
+
+  add_subdirectory("libs/jsoncpp")
+  target_link_libraries(${PACKAGE_NAME} ocpn::jsoncpp)
+endmacro ()
